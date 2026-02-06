f08cd7a5117a99ffdc4b43194985cbc3166a2a29
shirokuro(419087) <kazuki419087@gmail.com>
2026-02-06 20:02:45 +0900

    feat: ファイルや命名規則などの整理

--- Assets/latency-vr-interaction-experiment/Scripts/VRRigFollower.cs
+++ Assets/latency-vr-interaction-experiment/Scripts/VRRigFollower.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class VRRigFollower : MonoBehaviour
 {
-    [SerializeField] private Transform target; // 追従対象（XR Controllerなど）
-    [SerializeField] private Vector3 positionOffset;
-    [SerializeField] private Vector3 rotationOffset;
+    [SerializeField] private Transform _target; // 追従対象（XR Controllerなど）
+    [SerializeField] private Vector3 _positionOffset;
+    [SerializeField] private Vector3 _rotationOffset;
 
     void LateUpdate() // カメラの動きの後に実行
     {
         // 座標と回転を同期させる
-        transform.position = target.position + target.TransformDirection(positionOffset);
-        transform.rotation = target.rotation * Quaternion.Euler(rotationOffset);
+        transform.position = _target.position + _target.TransformDirection(_positionOffset);
+        transform.rotation = _target.rotation * Quaternion.Euler(_rotationOffset);
     }
 }
