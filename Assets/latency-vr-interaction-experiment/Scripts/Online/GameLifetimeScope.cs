93e9d913c1b86e77bf5e6440bf6018daed938ca9
shirokuro(419087) <kazuki419087@gmail.com>
2026-02-13 20:52:48 +0900

    wip: アバターを動的に生成する仕組みの実装途中
    アバターの動きをNetworkTransformなどで同期する仕組みが未実装

--- Assets/latency-vr-interaction-experiment/Scripts/Online/GameLifetimeScope.cs
+++ Assets/latency-vr-interaction-experiment/Scripts/Online/GameLifetimeScope.cs
@@ -2,22 +2,33 @@ using UnityEngine;
 using Unity.Netcode;
 using VContainer;
 using VContainer.Unity;
+using Unity.VisualScripting;
 
 public class GameLifetimeScope : LifetimeScope
 {
     [SerializeField] private NetworkManager _networkManager;
     [SerializeField] private ConnectionManager _connectionManagerPrefab;
 
+    [Header("VR Rig Target Settings")]
+    [SerializeField] private CameraMarker _cameraMarker;
+    [SerializeField] private LeftControllerMarker _leftControllerMarker;
+    [SerializeField] private RightControllerMarker _rightControllerMarker;
+
+
     [Header("Network Settings")]
-    [SerializeField] private GameObject _playerPrefab;
+    [SerializeField] private PlayerInitializer _playerPrefab;   // IPlayerPrefabMarkerを実装している必要がある(差し替え後に具象クラス型に変更)
+
     [SerializeField] private int _maxClients = 2;
 
     protected override void Configure(IContainerBuilder builder)
     {
         builder.RegisterComponent(_networkManager);
 
-        var config = new NetworkConfigData(_playerPrefab, _maxClients);
-        builder.RegisterInstance(config);
+        var vrConfig = new VRConfigData(_cameraMarker, _leftControllerMarker, _rightControllerMarker);
+        builder.RegisterInstance(vrConfig);
+
+        var networkConfig = new NetworkConfigData(_playerPrefab, _maxClients);
+        builder.RegisterInstance(networkConfig);
 
         builder.Register<PlayerSpawner>(Lifetime.Singleton);
 
