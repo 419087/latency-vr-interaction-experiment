233454989cd038e73d41793a5ef6926424c2b360
shirokuro(419087) <kazuki419087@gmail.com>
2026-02-18 21:48:48 +0900

    wip: 両手を含むタッチするタスクの実装途中

--- Assets/latency-vr-interaction-experiment/Scripts/Flow/ITaskExcuter.cs
+++ Assets/latency-vr-interaction-experiment/Scripts/Flow/ITaskExcuter.cs
@@ -4,5 +4,5 @@ using Cysharp.Threading.Tasks;
 public interface ITaskExcuter
 {
     UniTask ExecuteTask(HandSide handSide);
-    void CompleteCurrentTask(HandSide handSide1, HandSide handSide2);
+    void TouchedHand(HandSide handSide1, HandSide handSide2);
 }
