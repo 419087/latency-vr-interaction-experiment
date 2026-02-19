a2b31f7586cde0947bb31fe9c4c89854c4793480
shirokuro(419087) <kazuki419087@gmail.com>
2026-02-19 20:47:47 +0900

    feat: タスクのインターバルを設定する機能を追加

--- Assets/latency-vr-interaction-experiment/Scripts/Flow/GameManager.cs
+++ Assets/latency-vr-interaction-experiment/Scripts/Flow/GameManager.cs
@@ -1,18 +1,21 @@
-using System.Threading.Tasks;
+using System;
 using UnityEngine;
 using VContainer;
-using VIVE.OpenXR;
+using Cysharp.Threading.Tasks;
 
 public class GameManager : MonoBehaviour
 {
     private TaskCreator _taskCreator;
     private ITaskExcuter _taskExcuter;
 
+    private (int min, int max) _taskIntervalRange;
+
     [Inject]
-    public void Construct(TaskCreator taskCreator, ITaskExcuter taskExcuter)
+    public void Construct(TaskCreator taskCreator, ITaskExcuter taskExcuter, TaskConfigData taskConfig)
     {
         _taskCreator = taskCreator;
         _taskExcuter = taskExcuter;
+        _taskIntervalRange = (taskConfig.MinIntervalMilliSeconds, taskConfig.MaxIntervalMilliSeconds);
     }
 
     public async void StartGame()
@@ -23,6 +26,10 @@ public class GameManager : MonoBehaviour
 
         for (int i = 0; i < tasks.Count; i++)
         {
+
+            int randomMilliseconds = UnityEngine.Random.Range(_taskIntervalRange.min, _taskIntervalRange.max);
+            await UniTask.Delay(TimeSpan.FromMilliseconds(randomMilliseconds));
+
             await _taskExcuter.ExecuteTask(tasks[i]);
         }
 
--- Assets/latency-vr-interaction-experiment/Scripts/Flow/TaskConfigData.cs
+++ Assets/latency-vr-interaction-experiment/Scripts/Flow/TaskConfigData.cs
@@ -2,9 +2,13 @@ using UnityEngine;
 public class TaskConfigData
 {
     public int TaskIterations { get; }
+    public int MinIntervalMilliSeconds { get; }
+    public int MaxIntervalMilliSeconds { get; }
 
-    public TaskConfigData(int taskIterations)
+    public TaskConfigData(int taskIterations, int minIntervalMilliSeconds, int maxIntervalMilliSeconds)
     {
         TaskIterations = taskIterations;
+        MinIntervalMilliSeconds = minIntervalMilliSeconds;
+        MaxIntervalMilliSeconds = maxIntervalMilliSeconds;
     }
 }
